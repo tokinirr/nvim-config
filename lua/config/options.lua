230509e2561e2f00efabe2372015e1544f6d6902
tokinirr <tnirina32@gmail.com>
2026-02-15 05:13:59 +0000

    options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.wrap = true
-vim.opt.linebreak = true
-vim.opt.breakindent = true
-vim.opt.scrolloff = 5
+local opt = vim.opt
+
+opt.tabstop = 4 -- 4 espacs pour les tabulations
+opt.shiftwidth = 4 -- 4 espaces pour les indentations
+opt.expandtab = false -- ne change les tabulations en espaces
+opt.autoindent = true -- on garde l'indentation actuel pour la prochaine ligne
+opt.wrap = true
+opt.linebreak = true
+opt.breakindent = true
+opt.scrolloff = 5
